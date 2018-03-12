e62a03f75dfafc2f71954858d9a0b51512b1febe
Charlotte FARJANEL <cfarjane@e2r5p15.42.fr>
2018-03-12 19:37:56 +0100

    finaly

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cfarjane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/22 11:47:29 by cfarjane          #+#    #+#             */
-/*   Updated: 2018/03/07 17:30:23 by cfarjane         ###   ########.fr       */
+/*   Updated: 2018/03/12 19:35:44 by cfarjane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ static char		*ft_read(char *str, int fd)
 	char	buff[BUFF_SIZE + 1];
 
 	i = 0;
+	str = NULL;
 	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[ret] = '\0';
@@ -35,25 +36,17 @@ static char		*ft_read(char *str, int fd)
 	return (str);
 }
 
-static int		error(int fd, char **str, char **line)
-{
-	if (fd < 0 || line == NULL || read(fd, &str, 0) == -1)
-		return (-1);
-	if (!(*line = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1))))
-		return (-1);
-	return (0);
-}
-
 static char		*ft_strccpy(char *str, char c)
 {
 	char	*new;
 	int		i;
 
-	i = 0;
 	if (!str)
 		return (NULL);
-	if (!(new = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1))))
+	i = ft_strchr(str, c) ? (int)(ft_strchr(str, c) - str) : ft_strlen(str);
+	if (!(new = (char*)malloc(sizeof(char) * (i + 1))))
 		return (NULL);
+	i = 0;
 	while (str[i] && str[i] != c)
 	{
 		new[i] = str[i];
@@ -69,27 +62,24 @@ int				get_next_line(const int fd, char **line)
 	char		*tmp;
 	int			i;
 
-	if (error(fd, &str, line) == -1)
+	if (fd < 0 || line == NULL || read(fd, str, 0) == -1)
 		return (-1);
-	if (str)
-		ft_strcpy(*line, str);
-	else
+	if (!str)
 		str = ft_read(*line, fd);
 	i = 0;
-	if (str[i])
+	if (str && str[i])
 	{
 		while (str[i] && str[i] != '\n' && str[i + 1] != '\0')
 			i++;
-		if (i == 0)
-			*line = ft_strdup("");
-		else
-		{
-			*line = ft_strccpy(str, '\n');
-			tmp = str;
+		*line = ft_strccpy(str, '\n');
+		tmp = str;
+		if (*(str + i + 1))
 			str = ft_strdup(&str[i + 1]);
-			free(tmp);
-		}
+		else
+			str = NULL;
+		free(tmp);
 		return (1);
 	}
+	str = NULL;
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: cfarjane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/31 16:54:48 by cfarjane          #+#    #+#             */
-/*   Updated: 2018/03/05 01:16:02 by cfarjane         ###   ########.fr       */
+/*   Updated: 2018/03/12 19:36:19 by cfarjane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 64
+# define BUFF_SIZE 100
 # include <unistd.h>
 # include <stdlib.h>
 # include <sys/types.h>
